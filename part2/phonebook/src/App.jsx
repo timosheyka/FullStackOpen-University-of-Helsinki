67963d1f81dc9af9197a3cf40027bc4de2fe546b
timosheyka <kulltima820@gmail.com>
2024-06-11 15:54:24 +0200

    completed phonebook task

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,16 +1,17 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import personsService from './services/persons'
 
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
+import Notification from './components/Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
+  const [message, setMessage] = useState(null)
 
   useEffect(() => {
     personsService
@@ -22,42 +23,51 @@ const App = () => {
     event.preventDefault()
 
     const person = persons.find(person => person.name === newName)
+    console.log(person);
 
-    if (person && newNumber === person.number) {
-      alert(`\'${newName}\' is already in phonebook`)
-    } else if (newNumber === '') {
-      alert(`please enter the number`)
-    } else if (newNumber !== person.number) {
-      if (window.confirm(`Replace ${person.name} number ?`)) {
-        const updatedNumber = { ...person, number: newNumber }
-        personsService
-          .update(person.id, updatedNumber)
-          .then(returnedPerson => {
-            setPersons(
-              persons.map((p) => {
-                return p.id !== person.id ? p : returnedPerson;
-              })
-            )
-            setNewName('')
-            setNewNumber('') 
-          })
-          .catch(error => {
-            alert(`the person '${newName}' was already deleted from server`)
-            setPersons(persons.filter(n => n.id !== id))
-          })
+    if (person) {
+      if (newNumber === person.number) {
+        setMessage(`\'${newName}\' is already in phonebook`)
+      } else {
+        if (window.confirm(`Replace ${person.name} number ?`)) {
+          const updatedNumber = { ...person, number: newNumber }
+          personsService
+            .update(person.id, updatedNumber)
+            .then(returnedPerson => {
+              setPersons(
+                persons.map((p) => {return p.id !== person.id ? p : returnedPerson;})
+              )
+              setNewName('')
+              setNewNumber('') 
+            })
+            .catch(error => {
+              alert(`the person '${newName}' was already deleted from server`)
+              setPersons(persons.filter(n => n.id !== id))
+            })
+        }
       }
     } else {
-      personsService
-      .create({ name: newName, number: newNumber })
-        .then(returnedPerson => {
-          setPersons(persons.concat(returnedPerson))
-          setNewName('')
-          setNewNumber('') 
-        })
-        .catch(error => {
-          alert(`the person '${newName}' was already deleted from server`)
-          setPersons(persons.filter(n => n.id !== id))
-        })
+      if (newName === '') {
+        setMessage(`please enter the name`)
+      } else {
+        if (newNumber === '') {
+          setMessage(`please enter the number`)
+        } else {
+          personsService
+          .create({ name: newName, number: newNumber })
+            .then(returnedPerson => {
+              setPersons(persons.concat(returnedPerson))
+              setMessage(`Added ${newName}`)
+              setTimeout(() => { setMessage(null) }, 5000)
+              setNewName('')
+              setNewNumber('') 
+            })
+            .catch(error => {
+              alert(`the person '${newName}' was already deleted from server`)
+              setPersons(persons.filter(n => n.id !== id))
+            })
+        }
+      }
     }
   }
 
@@ -83,6 +93,7 @@ const App = () => {
         onChange={(event) => { setFilterName(event.target.value) }}
       />
       <h3>Add a new Person</h3>
+      <Notification message={message}/>
       <PersonForm
         newName={newName}
         onChangeName={(event) => { setNewName(event.target.value) }}
