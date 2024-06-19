c2d8203bbd05b955c7a6c0fb72eebf2303b6c2f9
timosheyka <kulltima820@gmail.com>
2024-06-19 17:55:28 +0200

    completed phonebook task

---
+++ part3/phone/phonebook/eslint.config.mjs
@@ -0,0 +1,18 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+export default [
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  {
+    ignores: ['dist/**', 'node_modules/**']
+  },
+  {
+    rules: {
+      'no-unused-vars': 0,
+      indent: ['error', 2],
+      quotes: ['error', 'single']
+    }
+  }
+];
